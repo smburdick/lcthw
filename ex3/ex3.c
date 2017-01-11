f8929e2bba4f139e5cf0d0ecf5389ddf92eff4d3
Sam Burdick <smburdick@pugetsound.edu>
2017-01-10 20:22:18 -0800

    ex3

---
+++ ex3/ex3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc , char *argv[])
+{
+  int age = 90;
+  int height = 72;
+
+  printf("I am %x years old\n", age);
+  printf("I am %i inches tall\n", height);
+
+  return 0;
+}
